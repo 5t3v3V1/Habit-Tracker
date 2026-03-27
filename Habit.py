2bcb666608e1d18d557e18ae88ce0d873ee4a54b
zxnIex <stevennguyen1122@icloud.com>
2026-03-27 09:59:20 +0000

    Save and ad function created, saves to a json file

--- Habit.py
+++ Habit.py
@@ -5,15 +5,21 @@ def load_habits():
 
     try:
         with open("habits.json", "r", encoding="utf-8") as file:
-            habit_dictionary = json.load(file)
-            return habit_dictionary
+            habit_dictionary = json.load(file)       
     
     except FileNotFoundError:
         pass
+
+    return habit_dictionary
 
 habit_dictionary = load_habits()
 
 def save_habits(habit_dictionary, no_habits):
     with open("habits.json", "w", encoding="utf-8") as file:
-        habit_json = json.dump(habit_dictionary, file, indent=no_habits)
+        habit_json = json.dump(habit_dictionary, file, indent = no_habits)
+
+def add_habit(habit_dictionary):
+    habit = input("Habit: ")
+    habit_dictionary[habit] = []
+    return habit_dictionary
         
